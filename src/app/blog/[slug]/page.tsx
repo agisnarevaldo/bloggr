2183a4c6fd2e47b3584c0b2c7e2dd693fcc64b69
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-09 19:51:02 +0700

    feat: Update PostPage to generate metadata for blog posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,33 +1,58 @@
-    import Heading from "@/components/heading";
-    import Image from "next/image";
-    import {getPost} from "@/lib/post";
+import Heading from "@/components/heading";
+import Image from "next/image";
+import {getPost} from "@/lib/post";
+import {Metadata} from "next";
 
-    interface PostPageProps {
-        params: {
-            slug: string;
-        };
+type MetaDataParams = {
+    params: {
+        slug: string,
     }
+}
 
-    export default async function PostPage({params: {slug}}: PostPageProps) {
-        const post = await getPost(slug);
+type Post = {
+    title: string;
+    description: string;
+    date: string;
+    author: string;
+    image: string;
+    body: string | Promise<string>
+}
+
+export async function generateMetadata( { params: { slug } }: MetaDataParams) : Promise<Metadata> {
+    const post = await getPost(slug);
+
+    return {
+        title: post.title,
+        description: post.description,
+    };
+}
+
+interface PostPageProps {
+    params: {
+        slug: string;
+    };
+}
+
+export default async function PostPage({params: {slug}}: PostPageProps) {
+    const post: Post = await getPost(slug);
 
     return (
-            <div>
-                <Heading>{post.title}</Heading>
-                <p className="italic text-sm pb-2">
-                    {post.date} by {post.author}
-                </p>
+        <div className="max-w-screen-sm mx-auto pt-4">
+            <Heading>{post.title}</Heading>
+            <p className="italic text-sm pb-2">
+                {post.date} by {post.author}
+            </p>
             <Image
-                    src={post.image}
-                    alt="Blog Thumbnail"
-                    width={640}
-                    height={360}
-                    className="mb-2 rounded-lg"
-                />
-                <article
-                    dangerouslySetInnerHTML={{__html: post.body}}
-                    className="max-w-screen-sm prose dark:prose-dark"
-                />
-            </div>
-        )
-    }
+                src={post.image}
+                alt="Blog Thumbnail"
+                width={640}
+                height={360}
+                className="mb-2 rounded-lg"
+            />
+            <article
+                dangerouslySetInnerHTML={{__html: post.body}}
+                className="max-w-screen-sm prose dark:prose-dark"
+            />
+        </div>
+    )
+}

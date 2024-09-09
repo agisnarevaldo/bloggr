2bf99622fd1ea46d17a4e7fdf54889410fb1497a
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-09 19:51:29 +0700

    chore: Remove commented out code and unused function from BlogPage component

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,5 @@
 import Heading from "../../components/heading";
-import Pagination from "../../components/pagination";
+// import Pagination from "../../components/pagination";
 import PostCard from "../../components/postCard";
 import {getAllPost} from "../../lib/post";
 
@@ -7,19 +7,20 @@ export const revalidate = 30;
 
 export const metadata = {
     title: "Blog",
+    description: "List of blog posts",
 };
 
 export default async function BlogPage() {
     const posts = await getAllPost();
-    console.log(posts);
+    // console.log(posts);
     return (
-        <>
+        <div className="max-w-screen-sm mx-auto pt-4">
             <Heading>Blog</Heading>
             <h2 className="text-2xl mb-3">List of posts</h2>
             {/*<Pagination href="/blog" page={page} pageCount={pageCount} />*/}
-            {posts.map((post, index) => (   
+            {posts.map((post, index) => (
                 <PostCard
-                key={index} // Use index as key because the slug is not unique  // Use index as key because the slug is not unique
+                key={index} // Use index as key because the slug is not unique
                     title={post.title}
                     href={`/blog/${post.slug}`}
                     image={post.image}
@@ -28,16 +29,16 @@ export default async function BlogPage() {
                     author={post.author}
                 />
             ))}
-        </>
+        </div>
     );
 }
-
-function parsePageParam(paramValue) {
-    if (paramValue) {
-        const page = parseInt(paramValue);
-        if (isFinite(page) && page > 0) {
-            return page;
-        }
-    }
-    return 1;
-}
+//
+// function parsePageParam(paramValue) {
+//     if (paramValue) {
+//         const page = parseInt(paramValue);
+//         if (isFinite(page) && page > 0) {
+//             return page;
+//         }
+//     }
+//     return 1;
+// }

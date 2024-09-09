b6cbe5b83efc32f5ce23b7e7b11f713cffdfe50f
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-09 19:52:07 +0700

    refactor: Update Home page layout and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
   import Image from "next/image";
   import Link from "next/link";
 
+  export const metadata = {
+      title: "Home",
+  }
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
           alt="Next.js logo"
-          width={180}
+          width={150}
           height={38}
           priority
         />
-          <h1 className="text-4xl font-bold">Hello World</h1>
-          <p className="text-lg text-center sm:text-left">Bloggr</p>
+          <h1 className="text-4xl font-bold">Bloggr</h1>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <Link href="https://github.com/agisnarevaldo    ">Agisna Revaldo</Link>
+      <footer className="row-start-5 flex flex-wrap items-center justify-center">
+                  <Link href="https://github.com/agisnarevaldo">&copy;2024 Agisna Revaldo</Link>
       </footer>
     </div>
   );

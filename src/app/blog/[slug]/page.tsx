8127061842c1ad74e448e249b85d05af372c663a
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-10 23:07:57 +0700

    feat: Add lazy loading for images in BlogPage component

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,7 +1,13 @@
 import Heading from "@/components/heading";
 import Image from "next/image";
-import {getPost} from "@/lib/post";
+import {getPost, getSlugs} from "@/lib/post";
 import {Metadata} from "next";
+import ShareLinkButton from "@/components/shareLinkButton";
+
+export async function generateStaticParams() {
+    const slugs = await getSlugs();
+    return slugs.map((slug) => ({slug}));
+}
 
 type MetaDataParams = {
     params: {
@@ -39,15 +45,18 @@ export default async function PostPage({params: {slug}}: PostPageProps) {
     return (
         <div className="max-w-screen-sm mx-auto pt-4">
             <Heading>{post.title}</Heading>
-            <p className="italic text-sm pb-2">
-                {post.date} by {post.author}
-            </p>
+            <div className="flex gap-3 pb-2 items-baseline">
+                <p className="italic text-sm pb-2">
+                    {post.date} by {post.author}
+                </p>
+                <ShareLinkButton />
+            </div>
             <Image
                 src={post.image}
                 alt="Blog Thumbnail"
-                width={640}
-                height={360}
-                className="mb-2 rounded-lg"
+                width={540}
+                height={260}
+                className="mb-2 rounded-lg mx-auto"
             />
             <article
                 dangerouslySetInnerHTML={{__html: post.body}}

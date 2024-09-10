5cbcea14826b5b607ce7d98d57970e8cc9eab66b
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-10 23:08:06 +0700

    refactor: Update ShareLinkButton component to improve accessibility and styling

--- src/components/shareLinkButton.tsx
+++ src/components/shareLinkButton.tsx
@@ -1,20 +1,23 @@
+"use client";
+
 import {useState} from "react";
 import {Icon} from "@iconify/react";
 
 export default function ShareLinkButton() {
     const [copied, setCopied] = useState(false);
+
     function handleClick() {
         navigator.clipboard.writeText(window.location.href);
         setCopied(true);
-    setTimeout(() => setCopied(false), 2000);
+        setTimeout(() => setCopied(false), 2000);
     }
 
     return (
         <button
             onClick={handleClick}
-            className="flex items-center gap-2 p-2 text-sm text-gray-500 bg-gray-100 rounded-md hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-gray-500"
+            className="flex items-center gap-2 p-1 text-sm text-slate-200 rounded-md hover:bg-gray-400 hover:text-slate-950 focus:outline-none focus-visible:ring focus-visible:ring-gray-500"
         >
-            <Icon icon="tabler:copy" />
+            <Icon icon="tabler:copy"/>
             {copied ? "Copied!" : "Copy link"}
         </button>
     )

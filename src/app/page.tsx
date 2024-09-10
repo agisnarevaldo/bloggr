1d5e267b5f6faf7995ab2c0bf0eff6ec05248dea
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-10 23:07:49 +0700

    refactor: Update Home page layout and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,28 @@
-  import Image from "next/image";
-  import Link from "next/link";
+import Image from "next/image";
+import Link from "next/link";
 
-  export const metadata = {
-      title: "Home",
-  }
+export const metadata = {
+    title: "Home",
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={150}
-          height={38}
-          priority
-        />
-          <h1 className="text-4xl font-bold">Bloggr</h1>
-      </main>
-      <footer className="row-start-5 flex flex-wrap items-center justify-center">
-                  <Link href="https://github.com/agisnarevaldo">&copy;2024 Agisna Revaldo</Link>
-      </footer>
-    </div>
-  );
+    return (
+        <div
+            className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-4 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-2 row-start-2 items-center sm:items-start">
+                <Image
+                    className=""
+                    src="/logo.png"
+                    alt="Next.js logo"
+                    width={120}
+                    height={60}
+                    priority
+                />
+                <h1 className="text-4xl font-bold">Bloggr</h1>
+            </main>
+            <footer className="row-start-4 flex flex-wrap items-center justify-center">
+                <Link href="https://github.com/agisnarevaldo">&copy;2024 Agisna Revaldo</Link>
+            </footer>
+        </div>
+    );
 }

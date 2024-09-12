349dbd408ceaf3f7de57d7223354dd74aaa06207
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-09-12 23:07:29 +0700

    refactor: Update Strapi request to include query parameters for fields, population, sorting, and pagination

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,6 +1,14 @@
 import {writeFileSync} from "node:fs";
+import qs from "qs";
 
-const url = "http://localhost:1337/api/posts" + "?populate=*";
+const url = "http://localhost:1337/api/posts" + "?" + qs.stringify({
+    fields: ["slug", "title", "description", "publishedAt", "author", "body"],
+    populate: {image: {fields: ["url"]}},
+    sort: ["publishedAt:desc"],
+    pagination: { pageSize: 6},
+}, {
+    encodeValuesOnly: true
+});
 const response = await fetch(url);
 const body = await response.json();
 const posts = JSON.stringify(body, null, 2);
